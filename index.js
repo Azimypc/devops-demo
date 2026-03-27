a84ab07bc9b5dc88b7dc1cedfd74070763d0c902
Mohammad Azimi <xsmax3@web.de>
2026-03-27 23:11:37 +0100

    updata

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello DevOps Pamirsoftware IT Solutions 🚀");
+  res.send("Fuck  DevOps Pamirsoftware IT Solutions 🚀");
   
 });
 
